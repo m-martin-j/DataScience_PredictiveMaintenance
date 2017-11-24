92e25df229f162e1164de8859ff47997fcf35f48
Daniel Geiger <daniel@ge1ger.de>
2017-11-24 17:43:12 +0100

    join on PK_DiagnosticDatasetDefinition

--- SQL/JoinDiagnosticDataSetDescription.sql
+++ SQL/JoinDiagnosticDataSetDescription.sql
@@ -1,7 +1,7 @@
 /****** Skript für SelectTopNRows-Befehl aus SSMS ******/
 SELECT FK_ReadOut, StartDateTime, EndDateTime, AlarmCount, Latitude, Longitude, DiagnosticDataSetDefinition.Description_L1, EnvironmentDataSet.DigitalSignalValues, EnvironmentDataSet.AnalogSignalValues, DinGroup.Description_L1
   FROM DiagnosticDataSet 
-  JOIN DiagnosticDataSetDefinition ON (DiagnosticDataSet.FK_DiagnosticDataSetDefinition = DiagnosticDataSetDefinition.DefinitionNumber)
+  JOIN DiagnosticDataSetDefinition ON (DiagnosticDataSet.FK_DiagnosticDataSetDefinition = DiagnosticDataSetDefinition.PK_DiagnosticDatasetDefinition)
   JOIN EnvironmentDataSet ON (EnvironmentDataSet.FK_DiagnosticDataSet = DiagnosticDataSet.PK_DiagnosticDataSet)
   JOIN DinGroup ON (DinGroup.PK_DinGroup = DiagnosticDataSetDefinition.FK_DinGroup)
   ORDER BY DiagnosticDataSet.StartDateTime
