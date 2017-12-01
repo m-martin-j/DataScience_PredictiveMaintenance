6f66ec008cbddc43b77ffca77f9de45a516c0c3b
StaplerfahrerKlaus <ts.wiwi@gmail.com>
2017-12-01 12:13:14 +0100

    added dataframe generation and beautification for all dinGroups

--- R/mastertable.R
+++ R/mastertable.R
@@ -1,9 +1,10 @@
 library(RODBC)
-library(corrplot)
-library(Hmisc)
+#library(corrplot)
+#library(Hmisc)
 library(stringr)
 
-dbhandle <- odbcDriverConnect('driver={SQL Server};server=DESKTOP-VTDVFDB\\SQLEXPRESS;database=ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004;trusted_connection=true')
+dbhandle <- odbcDriverConnect('driver={SQL Server};server=KLAPPRECHNER;database=ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004;trusted_connection=true')
+
 #2034
 res2034 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -13,9 +14,10 @@ res2034 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 JOIN [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[ReadOut] ON ([ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet].[FK_ReadOut] = [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[ReadOut].[PK_ReadOut])
                 JOIN [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[Vehicle] ON ([ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[ReadOut].[FK_Vehicle] = [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[Vehicle].[PK_Vehicle])
                 
-                WHERE PK_Vehicle = 2 AND PK_DinGroup = 2034 AND Description_L1=\'Daten Allgemein\' ')
+                WHERE PK_Vehicle = 2 AND PK_DinGroup = 2034 AND [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSetDefinition].[Description_L1]=\'Daten Allgemein\' ')
 
-din2034 <- within(res2034, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2034 <- within(res2034, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2035
 res2035 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -27,7 +29,8 @@ res2035 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2035 ')
 
-din2035 <- within(res2035, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2035 <- within(res2035, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2036
 res2036 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -39,7 +42,8 @@ res2036 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2036 ')
 
-din2036 <- within(res2036, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2036 <- within(res2036, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2037
 res2037 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -51,7 +55,8 @@ res2037 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2037 ')
 
-din2037 <- within(res2037, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2037 <- within(res2037, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2038
 res2038 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -63,7 +68,8 @@ res2038 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2038 ')
 
-din2038 <- within(res2038, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2038 <- within(res2038, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2039
 res2039 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -75,7 +81,8 @@ res2039 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2039 ')
 
-din2039 <- within(res2039, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2039 <- within(res2039, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2040
 res2040 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -87,7 +94,8 @@ res2040 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2040 ')
 
-din2040 <- within(res2040, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2040 <- within(res2040, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2041
 res2041 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -100,6 +108,7 @@ res2041 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2041 ')
 
 din2041 <- within(res2041, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2042
 res2042 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -112,6 +121,7 @@ res2042 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2042 ')
 
 din2042 <- within(res2042, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2043
 res2043 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -124,6 +134,7 @@ res2043 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2043 ')
 
 din2043 <- within(res2043, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2044
 res2044 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -135,7 +146,8 @@ res2044 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2044 ')
 
-din2044 <- within(res2044, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+dinG2044 <- within(res2044, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2045
 res2045 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -148,6 +160,7 @@ res2045 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                     WHERE PK_Vehicle = 2 AND PK_DinGroup = 2045 ')
 
 din2045 <- within(res2045, AnalogSignalValues<-data.frame(do.call('rbind', lapply(strsplit(as.character(AnalogSignalValues), ';', fixed=TRUE), as.numeric))))
+
 #2046
 res2046 <- sqlQuery(dbhandle, 'SELECT AnalogSignalValues, DateTime
                 FROM [ConcertoDb_TIF_WA6358_59_b9500bbf-f52a-474a-92c5-b863ed31d004].[dbo].[DiagnosticDataSet] 
@@ -164,75 +177,117 @@ din2046 <- within(res2046, AnalogSignalValues<-data.frame(do.call('rbind', lappl
 close(dbhandle)
 
 # Nichtbenötigte Spalten löschen
-Vars <- colnames(din2034$AnalogSignalValues)
+Vars <- colnames(dinG2034$AnalogSignalValues)
 Namen <- c("X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2034$AnalogSignalValues <- din2034$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2034$AnalogSignalValues <- dinG2034$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2035$AnalogSignalValues)
+Vars <- colnames(dinG2035$AnalogSignalValues)
 Namen <- c("X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2035$AnalogSignalValues <- din2035$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2035$AnalogSignalValues <- dinG2035$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2036$AnalogSignalValues)
+Vars <- colnames(dinG2036$AnalogSignalValues)
 Namen <- c("X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2036$AnalogSignalValues <- din2036$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2036$AnalogSignalValues <- dinG2036$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2037$AnalogSignalValues)
+Vars <- colnames(dinG2037$AnalogSignalValues)
 Namen <- c("X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2037$AnalogSignalValues <- din2037$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2037$AnalogSignalValues <- dinG2037$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2038$AnalogSignalValues)
+Vars <- colnames(dinG2038$AnalogSignalValues)
 Namen <- c("X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2038$AnalogSignalValues <- din2038$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2038$AnalogSignalValues <- dinG2038$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2039$AnalogSignalValues)
+Vars <- colnames(dinG2039$AnalogSignalValues)
 Namen <- c("X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2039$AnalogSignalValues <- din2039$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2039$AnalogSignalValues <- dinG2039$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2040$AnalogSignalValues)
+Vars <- colnames(dinG2040$AnalogSignalValues)
 Namen <- c("X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2040$AnalogSignalValues <- din2040$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2040$AnalogSignalValues <- dinG2040$AnalogSignalValues[, !(Vars %in% Namen)]
 
-Vars <- colnames(din2044$AnalogSignalValues)
+Vars <- colnames(dinG2044$AnalogSignalValues)
 Namen <- c("X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20")
-din2044$AnalogSignalValues <- din2044$AnalogSignalValues[, !(Vars %in% Namen)]
+dinG2044$AnalogSignalValues <- dinG2044$AnalogSignalValues[, !(Vars %in% Namen)]
 
 # Nicht benötigte Frames löschen
 rm(res2034, res2035, res2036, res2037, res2038, res2039, res2040, res2041, res2042, res2043, res2044, res2045, res2046, din2041, din2042, din2043, din2045, din2046)
 
 # Umbenennen der Variablen
 
-colnames(din2034$AnalogSignalValues)[1] <- "Dieselfüllstand"
-colnames(din2034$AnalogSignalValues)[2] <- "Frischwassertank"
-colnames(din2034$AnalogSignalValues)[3] <- "Batterie 1/2 Spannung"
-colnames(din2034$AnalogSignalValues)[4] <- "Batterie 3 Spannung"
-colnames(din2034$AnalogSignalValues)[5] <- "Batterie 4 Spannung"
-colnames(din2034$AnalogSignalValues)[6] <- "Batterie 1/2 Strom"
-colnames(din2034$AnalogSignalValues)[7] <- "Batterie 3 Strom"
-colnames(din2035$AnalogSignalValues)[1] <- "Drehzahl M1"
-colnames(din2035$AnalogSignalValues)[2] <- "Kühlmitteltemp M1"
-colnames(din2035$AnalogSignalValues)[3] <- "Öldruck M1"
-colnames(din2036$AnalogSignalValues)[1] <- "Drehzahl M2"
-colnames(din2036$AnalogSignalValues)[2] <- "Kühlmitteltemp M2"
-colnames(din2036$AnalogSignalValues)[3] <- "Öldruck M2"
-colnames(din2037$AnalogSignalValues)[1] <- "Betriebsstund. M1"
-colnames(din2037$AnalogSignalValues)[2] <- "Betriebsstund. M2"
-colnames(din2037$AnalogSignalValues)[3] <- "Betriebsstund. Kran1"
-colnames(din2037$AnalogSignalValues)[4] <- "Betriebsstund. Kran2"
-colnames(din2037$AnalogSignalValues)[5] <- "Betriebsstund. Kran3"
-colnames(din2038$AnalogSignalValues)[1] <- "Hydrauliköltank Füllstand"
-colnames(din2038$AnalogSignalValues)[2] <- "Hydrauliköl Temp"
-colnames(din2039$AnalogSignalValues)[1] <- "Getr. Arbeits. Fahrdruck A"
-colnames(din2039$AnalogSignalValues)[2] <- "Getr. Arbeits. Fahrdruck B"
-colnames(din2039$AnalogSignalValues)[3] <- "Getr. Arbeits. Speisedruck"
-colnames(din2040$AnalogSignalValues)[1] <- "Partikelf. Regenerationh"
-colnames(din2040$AnalogSignalValues)[2] <- "Partikelf. Filterwiderstand"
-colnames(din2040$AnalogSignalValues)[3] <- "Partikelf. Regenerations"
-colnames(din2044$AnalogSignalValues)[1] <- "Getr. Überst. Öltemp. 1"
-colnames(din2044$AnalogSignalValues)[2] <- "Getr. Überst. Öltemp. 2"
-colnames(din2044$AnalogSignalValues)[3] <- "Getr. Überst. Retarder1"
-colnames(din2044$AnalogSignalValues)[4] <- "Getr. Überst. Retarder2"
-colnames(din2044$AnalogSignalValues)[5] <- "Getr. Überst. Fahrdruck A"
-colnames(din2044$AnalogSignalValues)[6] <- "Getr. Überst. Fahrdruck B"
-colnames(din2044$AnalogSignalValues)[7] <- "Getr. Überst. Speisedruck"
+colnames(dinG2034$AnalogSignalValues)[1] <- "Dieselfüllstand"
+colnames(dinG2034$AnalogSignalValues)[2] <- "Frischwassertank"
+colnames(dinG2034$AnalogSignalValues)[3] <- "Batterie 1/2 Spannung"
+colnames(dinG2034$AnalogSignalValues)[4] <- "Batterie 3 Spannung"
+colnames(dinG2034$AnalogSignalValues)[5] <- "Batterie 4 Spannung"
+colnames(dinG2034$AnalogSignalValues)[6] <- "Batterie 1/2 Strom"
+colnames(dinG2034$AnalogSignalValues)[7] <- "Batterie 3 Strom"
+colnames(dinG2035$AnalogSignalValues)[1] <- "Drehzahl M1"
+colnames(dinG2035$AnalogSignalValues)[2] <- "Kühlmitteltemp M1"
+colnames(dinG2035$AnalogSignalValues)[3] <- "Öldruck M1"
+colnames(dinG2036$AnalogSignalValues)[1] <- "Drehzahl M2"
+colnames(dinG2036$AnalogSignalValues)[2] <- "Kühlmitteltemp M2"
+colnames(dinG2036$AnalogSignalValues)[3] <- "Öldruck M2"
+colnames(dinG2037$AnalogSignalValues)[1] <- "Betriebsstund. M1"
+colnames(dinG2037$AnalogSignalValues)[2] <- "Betriebsstund. M2"
+colnames(dinG2037$AnalogSignalValues)[3] <- "Betriebsstund. Kran1"
+colnames(dinG2037$AnalogSignalValues)[4] <- "Betriebsstund. Kran2"
+colnames(dinG2037$AnalogSignalValues)[5] <- "Betriebsstund. Kran3"
+colnames(dinG2038$AnalogSignalValues)[1] <- "Hydrauliköltank Füllstand"
+colnames(dinG2038$AnalogSignalValues)[2] <- "Hydrauliköl Temp"
+colnames(dinG2039$AnalogSignalValues)[1] <- "Getr. Arbeits. Fahrdruck A"
+colnames(dinG2039$AnalogSignalValues)[2] <- "Getr. Arbeits. Fahrdruck B"
+colnames(dinG2039$AnalogSignalValues)[3] <- "Getr. Arbeits. Speisedruck"
+colnames(dinG2040$AnalogSignalValues)[1] <- "Partikelf. Regenerationh"
+colnames(dinG2040$AnalogSignalValues)[2] <- "Partikelf. Filterwiderstand"
+colnames(dinG2040$AnalogSignalValues)[3] <- "Partikelf. Regenerations"
+colnames(dinG2044$AnalogSignalValues)[1] <- "Getr. Überst. Öltemp. 1"
+colnames(dinG2044$AnalogSignalValues)[2] <- "Getr. Überst. Öltemp. 2"
+colnames(dinG2044$AnalogSignalValues)[3] <- "Getr. Überst. Retarder1"
+colnames(dinG2044$AnalogSignalValues)[4] <- "Getr. Überst. Retarder2"
+colnames(dinG2044$AnalogSignalValues)[5] <- "Getr. Überst. Fahrdruck A"
+colnames(dinG2044$AnalogSignalValues)[6] <- "Getr. Überst. Fahrdruck B"
+colnames(dinG2044$AnalogSignalValues)[7] <- "Getr. Überst. Speisedruck"
 
+## DateTime als gleichwertige Spalte setzen
+din2034 <- dinG2034$AnalogSignalValues
+din2034[8] <- dinG2034$DateTime
+din2035 <- dinG2035$AnalogSignalValues
+din2035[4] <- dinG2035$DateTime
+din2036 <- dinG2036$AnalogSignalValues
+din2036[4] <- dinG2036$DateTime
+din2037 <- dinG2037$AnalogSignalValues
+din2037[6] <- dinG2037$DateTime
+din2038 <- dinG2038$AnalogSignalValues
+din2038[3] <- dinG2038$DateTime
+din2039 <- dinG2039$AnalogSignalValues
+din2039[4] <- dinG2039$DateTime
+din2040 <- dinG2040$AnalogSignalValues
+din2040[4] <- dinG2040$DateTime
+din2044 <- dinG2044$AnalogSignalValues
+din2044[8] <- dinG2044$DateTime
+# Spalten Partikelfilter falsch benannt/vertauscht!
 
+## Spaltenname zu DateTime ändern
+colnames(din2034)[8] <- "DateTime"
+colnames(din2035)[4] <- "DateTime"
+colnames(din2036)[4] <- "DateTime"
+colnames(din2037)[6] <- "DateTime"
+colnames(din2038)[3] <- "DateTime"
+colnames(din2039)[4] <- "DateTime"
+colnames(din2040)[4] <- "DateTime"
+colnames(din2044)[8] <- "DateTime"
+
+# nicht benötigte Frames löschen
+rm(dinG2034, dinG2035, dinG2036, dinG2037, dinG2038, dinG2039, dinG2040, dinG2044)
+
+## Truncate DateTime
+din2034$DateTime <- trunc(din2034$DateTime, 'secs')
+din2035$DateTime <- trunc(din2035$DateTime, 'secs')
+din2036$DateTime <- trunc(din2036$DateTime, 'secs')
+din2037$DateTime <- trunc(din2037$DateTime, 'secs')
+din2038$DateTime <- trunc(din2038$DateTime, 'secs')
+din2039$DateTime <- trunc(din2039$DateTime, 'secs')
+din2040$DateTime <- trunc(din2040$DateTime, 'secs')
+din2044$DateTime <- trunc(din2044$DateTime, 'secs')
+
+#To-Do: Merge
